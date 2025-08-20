b1fd4c5a813c7d548d24ad31e1893182fbd82d6e
javachrist <contact@javachrist.fr>
2025-08-21 00:25:43 +0200

    Modification de l'icone

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       includeAssets: [
-        'logo16.png','logo32.png','logo48.png',
-        'logo192.png','logo512.png',
-        'favicon.ico','apple-touch-icon.png','logo.svg'
+        "logo16.png",
+        "logo32.png",
+        "logo48.png",
+        "logo192.png",
+        "logo512.png",
+        "favicon.ico",
+        "apple-touch-icon.png",
       ],
       manifest: {
-        name: 'IconGen PWA',
-        short_name: 'IconGen',
-        theme_color: '#3b82f6',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
+        name: "IconGen PWA",
+        short_name: "IconGen",
+        theme_color: "#3b82f6",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
         icons: [
-          { src: '/logo192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
-          { src: '/logo512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
-          { src: '/logo.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any maskable' }
-        ]
-      }
-    })
+          {
+            src: "/logo192.png?v=1",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/logo512.png?v=1",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
-})
+});
